766d5766677ab83b6a73d8b2f1d8d8bdc7d841b1
pakhm95 <pakhj53@gmail.com>
2021-08-31 17:19:12 +0900

    0831-17:19

--- Coding/.vscode/C++_Python/baekjoon_11720.cpp
+++ Coding/.vscode/C++_Python/baekjoon_11720.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
-#include <math.h>
+#include <cmath>
 
 using namespace std;
 int main() {
     int input1;
+    string input2;
+    int R1 = 0;
     cin >> input1;
-
-    int input2;
     cin >> input2;
 
-    int R1 = input1 / pow(10,(input2 - 1));
-    cout << R1;
+    while (input1 > 0)
+    {
+        int N;
+        N = input2[--input1] - '0';
+        R1 += N;
+    }
     
+    cout << R1;
+
     return 0;
 }
