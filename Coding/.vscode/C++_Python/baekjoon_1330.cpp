cd941b50cf1666741e15e91c6928d698fb02665b
pakhm95 <pakhj53@gmail.com>
2021-09-01 10:27:08 +0900

    exe파일 삭제 및 1330업로드

---
+++ Coding/.vscode/C++_Python/baekjoon_1330.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int A, B;
+    cin >> A >> B;
+
+    if (A > B)
+    {
+        cout << ">" << "\n";
+        return 0;
+    }else if (A < B)
+    {
+        cout << "<" << "\n";
+        return 0;
+    }else if (A == B)
+    {
+        cout << "==" << "\n";
+        return 0;
+    }
+}
